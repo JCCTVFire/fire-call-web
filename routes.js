e1ebc7d938da41a4ddd695f227a0b31dfcb41007
Jonah Pool <jpool.school@gmail.com>
2021-03-21 16:29:30 -0400

    initial commit

---
+++ routes.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+
+import express from 'express';
+import fetch from 'fetch';
+import sequelize from 'sequelize';
+
+const router = express.Router();

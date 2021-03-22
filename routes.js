c00552bdf51747d383059d9c806a183d8638dc17
Jonah Pool <jpool.school@gmail.com>
2021-03-22 12:13:21 -0400

    remove outdated files

--- routes.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-
-import express from 'express';
-import fetch from 'fetch';
-import sequelize from 'sequelize';
-
-const router = express.Router();

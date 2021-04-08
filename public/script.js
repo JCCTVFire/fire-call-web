418e88250563f485eb3b8b7ffb33850376c60c54
vjpovlitz <vjpovlitz@gmail.com>
2021-04-08 13:40:09 -0400

    delete and get dates function

--- public/script.js
+++ public/script.js
@@ -1,13 +1,30 @@
-const router = express.Router();
+import { Json } from "sequelize/types/lib/utils";
 
+async function getDates() {
 
-function get_dates{
-
-    date_text = document.getElementById('button1');
-    
+    const date_text = document.getElementById('button1');
+    const data = {
+        start_date = new Date('2018-05-08');
+        end_date = new Date('2018-05-10');
+    }
+    const endpoint = '/api/incidents/on_dates';
+    const response = await fetch (endpoint, body: JSON.stringify(data);
+    return response.json();
 
 }
-/api/'/incidents/on_dates'
 
 
-date_range = document.getElementById("button1").value;
+
+async function testDelete() {
+    
+    const test_record = 1821899;
+    const data = {
+        incident_id: test_record
+
+    }
+    const delete_button = document.getElementById('button4');
+    const endpoint = '/api/incidents/incident_id';
+    const response = await fetch (endpoint, {method: 'DELETE', body: JSON.stringify(data)});
+    return response.json();
+}
+

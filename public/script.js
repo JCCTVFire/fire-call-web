e3b80453f7cfa39db9c743b4278c77f34889224e
vjpovlitz <vjpovlitz@gmail.com>
2021-04-08 13:01:12 -0400

    Update script.js

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const router = express.Router();
+
+
+function get_dates{
+
+    date_text = document.getElementById('button1');
+    
+
+}
+/api/'/incidents/on_dates'
+
+
+date_range = document.getElementById("button1").value;

4e0dd77268bb9cced1561f71b5ae868a0dae93ef
Jonah Pool <jpool.school@gmail.com>
2021-04-09 19:07:10 -0400

    fix server.js

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(staticFolder));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use('/api', routes);
 
 async function bootServer() {

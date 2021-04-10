8279d15e01fae99ce3323103f6ace632300c9949
Jonah Pool <jpool.school@gmail.com>
2021-04-09 20:36:16 -0400

    change to route format

--- server_files/routes/routes.js
+++ server_files/routes/routes.js
@@ -7,22 +7,43 @@ const router = express.Router();
 
 
 // INCIDENTS
-router.get('/incidents', async (req, res) => {
-  try {
-    const all_incidents = await db.incidents.findAll({
-      include: [
-        {
-          model: db.calls
-        }
-      ]
-    });
-    const reply = all_incidents.length > 0 ? {data: all_incidents} : {message: 'No results found.'};
-    res.json(reply);
-  } catch (err) {
-    console.error(err);
-    res.error('Server Error!');
-  }
-});
+router.route('/incidents')
+  .get(async (req, res) => {
+    try {
+      const all_incidents = await db.incidents.findAll();
+      const reply = all_incidents.length > 0 ? {data: all_incidents} : {message: 'No results found.'};
+      res.json(reply);
+    } catch (err) {
+      console.error(err);
+      res.error('Server Error!');
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const newIncident = await db.incidents.create({
+        incident_id:  req.body.incident_id,
+        date: req.body.date,
+        description: req.body.description,
+        postal_code: req.body.postal_code,
+        district_code: req.body.district_code,
+        call_id: req.body.call_id,
+        dispatch_id: req.body.dispatch_id
+      });
+      res.json(newIncident);
+    } catch (err) {
+      console.error(err);
+      res.error('Server Error!');
+    }
+  })
+  .put(async (req, res) => {
+    res.send('Action not available.');
+  })
+  .delete(async (req, res) => {
+    res.send('Action unavailable.');
+  });
+  
+  
+
 
 router.get('/incidents/on_dates', async (req, res) => {
   try {
@@ -33,6 +54,7 @@ router.get('/incidents/on_dates', async (req, res) => {
     const incidents = await db.incidents.findAll({
       where: {
         date: {
+          // [Op.placeholder]: ,
           [Op.between]: [startDate, endDate]
         }
       }
@@ -49,74 +71,74 @@ router.get('/incidents/on_dates', async (req, res) => {
   }
 });
 
-router.get('/incidents/:incident_id', async (req, res) => {
-  try {
-    const incident = await db.incidents.findAll({
-      where: {
-        incident_id: req.params.incident_id
-      }
-    });
-    res.json(incident);
-  } catch (err) {
-    console.error(err);
-    res.error('Server Error!');
-  }
-});
+router.route('/incidents/:incident_id')
+  .get(async (req, res) => {
+    try {
+      const incident = await db.incidents.findAll({
+        where: {
+          incident_id: req.params.incident_id
+        }
+      });
+      res.json(incident);
+    } catch (err) {
+      console.error(err);
+      res.error('Server Error!');
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const newIncident = await db.incidents.create({
+        incident_id:  req.params.incident_id,
+        date: req.body.date,
+        description: req.body.description,
+        postal_code: req.body.postal_code,
+        district_code: req.body.district_code,
+        call_id: req.body.call_id,
+        dispatch_id: req.body.dispatch_id
+      });
+      res.json(newIncident);
+    } catch (err) {
+      console.error(err);
+      res.error('Server Error!');
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      await db.incidents.update({
+        date: req.body.date,
+        description: req.body.description,
+        postal_code: req.body.postal_code,
+        district_code: req.body.district
+      },
+      {
+        where: {
+          incident_id: req.params.incident_id
+        }
+      });
+      res.send('Successful update.');
+    } catch (err) {
+      console.error(err);
+      res.error('Server Error!');
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      console.log(req.params)
+      await db.incidents.destroy({
+        where: {
+          incident_id: req.params.incident_id
+        }
+      });
+      res.send('Successful deletion.');
+    } catch (err) {
+      console.error(err);
+      res.error('Server Error!');
+    }
+  });
+
+  
 
 
-
-router.post('/incidents/new', async (req, res) => {
-  try {
-    const newIncident = await db.incidents.create({
-      incident_id:  req.body.incident_id,
-      date: req.body.date,
-      description: req.body.description,
-      postal_code: req.body.postal_code,
-      district_code: req.body.district_code,
-      call_id: req.body.call_id,
-      dispatch_id: req.body.dispatch_id
-    });
-    res.json(newIncident);
-  } catch (err) {
-    console.error(err);
-    res.error('Server Error!');
-  }
-});
-
-router.delete('/incidents/delete/:incident_id', async (req, res) => {
-  try {
-    console.log(req.body)
-    await db.incidents.destroy({
-      where: {
-        incident_id: req.params.incident_id
-      }
-    });
-    res.send('Successful deletion.');
-  } catch (err) {
-    console.error(err);
-    res.error('Server Error!');
-  }
-});
-
-router.put('/incidents/update/:incident_id', async (req, res) => {
-  try {
-    await db.incidents.update({
-      date: req.body.date,
-      description: req.body.description,
-      postal_code: req.body.postal_code,
-      district_code: req.body.district
-    },
-    {
-      where: {
-        incident_id: req.params.incident_id
-      }
-    });
-    res.send('Successful update.');
-  } catch (err) {
-    console.error(err);
-    res.error('Server Error!');
-  }
-})
 
 
 // // Gets the unit from an incident

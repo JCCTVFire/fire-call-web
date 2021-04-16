410422435cd90eafbb88b862bd9324ece76db4a9
ColinMannix <78052098+ColinMannix@users.noreply.github.com>
2021-04-16 19:46:03 -0400

    Added stations model

--- server_files/models/index.js
+++ server_files/models/index.js
@@ -2,11 +2,13 @@ import sequelize from 'sequelize';
 
 import calls from './calls.js';
 import dispatch from './dispatch.js';
+import stations from './stations.js';
 
 import incidents from './incidents.js'; // Parent of calls and dispatch. Must be defined first.
 
 export default {
     calls,
     dispatch,
-    incidents
+    incidents,
+    stations
 }

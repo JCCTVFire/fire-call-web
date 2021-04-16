c47ac75dac5d74772f229cc916e56f1195f17042
daitch-umd <78330551+daitch-umd@users.noreply.github.com>
2021-04-16 19:48:04 -0400

    Jurisdiction model and index added

--- server_files/models/index.js
+++ server_files/models/index.js
@@ -1,5 +1,15 @@
-import calls from './CallLog.js';
+import sequelize from 'sequelize';
+
+import calls from './calls.js';
+import dispatch from './dispatch.js';
+
+import incidents from './incidents.js'; // Parent of calls and dispatch. Must be defined first.
+import jurisdiction from './jurisdiction.js';
+
 
 export default {
     calls,
+    dispatch,
+    incidents, 
+    jurisdiction
 }

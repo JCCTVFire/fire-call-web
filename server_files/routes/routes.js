f7f4d337a2a15689ee48a6f6db343e1adf450455
Jonah Pool <jpool.school@gmail.com>
2021-04-16 22:15:24 -0400

    remove duplicate "calls" route

--- server_files/routes/routes.js
+++ server_files/routes/routes.js
@@ -201,27 +201,6 @@ router.route('/jurisdiction')
 // });
 
 
-// CALLS
-router.route('/calls')
-  .get(async (req, res) => {
-    try {
-      const all_calls = await db.calls.findAll();
-    } catch (err) {
-      console.error(err);
-      res.send('Server Error!');
-    }
-  })
-  .post(async (req, res) => {
-    res.send('Action not available.');
-  })
-  .put(async (req, res) => {
-    res.send('Action not available.');
-  })
-  .delete(async (req, res) => {
-    res.send('Action not available.');
-  });
-
-
 router.route('/incidents/:incident_id/dispatch')
   .get(async (req, res) => {
     res.send('Action not available');

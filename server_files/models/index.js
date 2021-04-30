c44b919926f7cd3856e98c30bb535ab46270aa9b
Jonah Pool <jpool.school@gmail.com>
2021-04-30 18:45:04 -0400

    Designate required models.

--- server_files/models/index.js
+++ server_files/models/index.js
@@ -2,17 +2,18 @@ import sequelize from 'sequelize';
 
 import calls from './calls.js';
 import dispatch from './dispatch.js';
-import incidents from './incidents.js'; // Parent of calls and dispatch. Must be defined first.
-
 import units from './units.js';
+import incidents from './incidents.js'; // Parent of calls, units, and dispatch. Must be defined first.
 
+import locations from './locations.js'; // Parent of incidents
+
+//////////// NOT USED ///////////////
 import incidents_has_units from './incidentsHasUnits.js'; // Join table for incidents and units
-
 import jurisdiction from './jurisdiction.js';
 import stations from './stations.js';
 import employees from './employees.js';
+/////////////////////////////////////
 
-import locations from './locations.js'; // Parent of incidents
 
 export default {
     calls,

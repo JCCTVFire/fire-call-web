8ce2ba2f99b5a2ae8fdc02f9d3973ddaa36b2c7b
Jonah Pool <jpool.school@gmail.com>
2021-05-03 14:38:46 -0400

    Add search functionality.

--- server_files/routes/routes.js
+++ server_files/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import sequelize from 'sequelize';
 import db from '../database/initDB.js';
+import incidents from '../models/incidents.js';
 const Op = sequelize.Op;
 const router = express.Router();
 
@@ -438,7 +439,7 @@ router.route('/units')
       const newUnit = await db.units.create({
         unit_id: req.body.unit_id,
         unit_number: req.body.unit_number,
-        unit_class: req.body.unit_class
+        unit_class_name: req.body.unit_class_name
       })
       res.send('New unit created!');
     } catch (err) {
@@ -475,7 +476,7 @@ router.route('/units/:unit_id')
     try {
       await db.units.update({
         unit_number: req.body.unit_number,
-        unit_class: req.body.unit_class
+        unit_class_name: req.body.unit_class_name
       },
       {
         where: {
@@ -508,7 +509,8 @@ router.route('/search')
 .get(async (req, res) => {
   try {
     // console.log(req)
-    let replyData = []
+    let incidentData = [];
+    let incidentIDs = [];
     const matchCalls = await db.calls.findAll({
       where: {
         [Op.or]: [
@@ -523,90 +525,113 @@ router.route('/search')
             }
           }
         ]
-      }
+      },
+      limit: 10
     });
-    let callsToIncidents;
-    if (matchCalls !== 'undefined') {
-      const call_ids = matchCalls.map((call) => {
+    if (matchCalls !== 'NULL') {
+      const callIDs = matchCalls.map((call) => {
         // console.log(call.dataValues.call_id);
         return call.dataValues.call_id;
       });
-      callsToIncidents = await db.incidents.findAll({
+      const callsToIncidents = await db.incidents.findAll({
         where: {
           call_id: {
-            [Op.in]: call_ids
+            [Op.in]: callIDs
           }
-        }
-      })
+        },
+      });
+      let a = 0;
+      callsToIncidents.map((unit) => {
+        incidentIDs.push(unit.dataValues.incident_id);
+        a += 1;
+      });
+      console.log("Calls:", a)
+      // console.log(typeof callsToIncidents, callsToIncidents);
     }
-    
+
     const matchUnits = await db.units.findAll({
       where: {
         [Op.or]: [
           {
             unit_number: {
-              [Op.iRegexp]: req.query.queryText
+              [Op.like]: req.query.queryText
             }
           },
           {
-            unit_class: {
-              [Op.iRegexp]: req.query.queryText
+            unit_class_name: {
+              [Op.like]: req.query.queryText
             }
           }
         ]
-      }
+      },
+      limit: 10
     });
     
-    let unitsToIncidents;
-    if (matchUnits !== 'undefined') {
-      const unit_ids = matchCalls.map((unit) => {
+    let unitsToIncidents = [];
+    console.log(typeof matchUnits, matchUnits);
+    if (matchUnits.length > 0) {
+      const unitIDs = matchUnits.map((unit) => {
         return unit.dataValues.unit_id
       });
 
-      unitsToIncidents = await db.incidents.findAll({
-        where: {
-          unit_id: {
-            [Op.in]: unit_ids
-          }
-        }
-      })
+      unitsToIncidents = await db.incidents.findAll({ where: { unit_id: { [Op.in]: unitIDs } } });
+      let b = 0;
+      unitsToIncidents.forEach((unit) => {
+        incidentIDs.push(unit.dataValues.incident_id);
+        b += 1;
+      });
+      console.log("Units:", b);
     }
 
-    console.log(`${req.query.endDate} 23:59:59`)
+    console.log(`${req.query.endDate} 23:59:59`);
     const startDate = Date.parse(`${req.query.startDate}T00:00:00-00:00`);
     const endDate = Date.parse(`${req.query.endDate}T23:59:59-00:00`);
 
     const matchIncidents = await db.incidents.findAll({
       where: {
-        [Op.and]: [{
-          [Op.or]: [
-            { 
-              description: {
-                [Op.startsWith]: req.query.queryText
-              } 
-            },
-            { 
-              postal_code: {
-                [Op.startsWith]: req.query.queryText
-              } 
-            },
-          ]},
+        [Op.and]: [
+          {
+            [Op.or]: [
+              {
+                incident_id: {
+                  [Op.in]: incidentIDs
+                }
+              },
+              { 
+                description: {
+                  [Op.startsWith]: req.query.queryText
+                } 
+              },
+              { 
+                postal_code: {
+                  [Op.startsWith]: req.query.queryText
+                } 
+              },
+            ]
+          },
           {
             date: {
               [Op.between]: [startDate, endDate]
             }
           }
         ]
-      }
+      },
+      limit: 10
     });
-
+    if (matchIncidents.length > 0) {
+      let c = 0;
+      matchIncidents.forEach((inc) => {
+        incidentData.push(inc.dataValues);
+        c += 1;
+      });
+    }
+    // console.log(matchIncidents);
     
-    console.log(matchIncidents);
-    const incidentData = matchIncidents + callsToIncidents;
     // console.log(incidentData);
-    replyData += [{'incidents': incidentData}];
+    // console.log(incidentData)
+    // console.log(incidentData);
     // console.log(replyData);
-    const reply = getReply(replyData);
+    const reply = getReply(incidentData);
     res.json(reply);
   } catch (err) {
     console.error(err);

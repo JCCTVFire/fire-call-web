d20373a511606056dfd680f85595f9f6c02e70a8
CodeGuru45 <eyeque27@gmail.com>
2021-05-03 00:02:33 -0400

    Deleted add call/manage call pages

--- public/script.js
+++ public/script.js
@@ -13,18 +13,6 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-
 function mapInit() {
     // follow the Leaflet Getting Started tutorial here
     document.getElementById('mapid').style.height = `${400}px`;

e8fae2c3f5e67281c011151392024d4e9925fa30
CodeGuru45 <eyeque27@gmail.com>
2021-05-03 17:38:42 -0400

    Corrected errors

--- server_files/models/locations.js
+++ server_files/models/locations.js
@@ -1,31 +1,31 @@
 export default (database, DataTypes) => {
-    const Locations = database.define(
-      'locations',
-      {
-        location_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true
-        },
-        lat: {
-          type: DataTypes.DECIMAL
-        },
-        long: {
-          type: DataTypes.DECIMAL
-        },
-        incidents_incident_id: {
-          type: DataTypes.INT
-        }
+  const Locations = database.define(
+    'locations',
+    {
+      locations_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
       },
-      {
-        freezeTableName: true, 
-        timestamps: false,
+      lat: {
+        type: DataTypes.DECIMAL
+      },
+      long: {
+        type: DataTypes.DECIMAL
+      },
+      incidents_incident_id: {
+        type: DataTypes.INTEGER
       }
-    );
-  
-    Locations.associate = function (db) {
-      Locations.hasOne(db.incidents);
-      db.incidents.belongsTo(Locations);
-    }; 
-    return Calls;
-  }
+    },
+    {
+      freezeTableName: true, 
+      timestamps: false,
+    }
+  );
+
+  Locations.associate = function (db) {
+    Locations.hasOne(db.incidents, {foreignKey: 'incident_id'});
+    db.incidents.belongsTo(Locations, {foreignKey: 'incident_id'});
+  }; 
+  return Locations;
+}
